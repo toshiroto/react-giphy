7ac48bae047daffd8531d5b653d5c390db6e062e
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-18 13:16:21 +0900

    React 101

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
+class Hello extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      clicked: false,
+      counter: 0
+    };
+  }
+
+  handleClicked = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
+    });
+  };
+
+  render () {
+    // Build and return HTML for the component.
+    return (
+      <div className={this.state.clicked ? 'clicked' : null}
+      onClick={this.handleClicked}>
+      Hello {this.props.name} {this.state.counter}
+      </div>
+    );
+  }
+
+}
+
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name="Carlos" />, root);
 }
